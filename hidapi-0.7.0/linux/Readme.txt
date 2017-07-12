91298bbe2a40bf0d27e1f2c70077ca9f1fd2575c
balaji200988 <balaji.tce.eee06@gmail.com>
2017-07-12 21:26:54 +0000

    This main is for open using vid and pid

---
+++ hidapi-0.7.0/linux/Readme.txt
@@ -0,0 +1,6 @@
+Extra things needed
+
+1. have to install a library called libudev*** 
+
+sudo apt-get install libudev-dev
+
--- hidapi-0.7.0/linux/main.c
+++ hidapi-0.7.0/linux/main.c
@@ -8,33 +8,30 @@
 #define MOUSE_VENDOR_ID  0x045e
 #define MOUSE_PRODUCT_ID 0x07fd
 
+#define MAX_STR_LEN      255
 void PrintDeviceInfo(struct hid_device_info *);
 
 
 int main()
 {
+	wchar_t wstr[MAX_STR_LEN];
+	int result;
 	hid_init(); // intialises the hid api
-	struct hid_device_info *hid_devices_pointer, *mouse_ptr; // pointers to struct hid_device_info to store the linked list starting addresses returned from hid_enumerate()
-	hid_devices_pointer =  hid_enumerate(0x0, 0x0) ; // enumerate all hid devices in laptop
-	PrintDeviceInfo(hid_devices_pointer); // prints information about devices, defined in hidExp.c
-	mouse_ptr = hid_enumerate(MOUSE_VENDOR_ID, MOUSE_PRODUCT_ID); // gets information about the mouse
-	PrintDeviceInfo(mouse_ptr); // prints information about the mouse
-
-	hid_free_enumeration(hid_devices_pointer); // free the memory
-	hid_free_enumeration(mouse_ptr); // free the memory
+	hid_device *DeviceHandle;
+	DeviceHandle = hid_open(MOUSE_VENDOR_ID, MOUSE_PRODUCT_ID, NULL);
+	if (DeviceHandle == NULL)
+	{
+		printf("Failed to open device\n");
+		return 1;
+	}
+	result = hid_get_product_string(DeviceHandle, wstr, MAX_STR_LEN);
+	if (result < 0)
+	{	
+		printf("Failed to get product string\n");
+		return 1;
+	}
+	printf("Manufacturer: %ls \n", wstr);
 	hid_exit(); // clean up 
 	return 0; // return
 }
 
-void PrintDeviceInfo(struct hid_device_info *temp)
-{
-	while(temp)
-	{
-		printf ("Device Found\n type: %04hx %04hx\n path: %s\n serial_number: %ls",
-			temp->vendor_id, temp->product_id, temp->path, temp->serial_number);
-		printf("\n");
-		printf("Manufacturer: %ls\n", temp->manufacturer_string);
-		printf("Product     : %ls\n\n", temp->product_string);
-		temp = temp->next;// go to the next object in the linked list
-	}
-}
